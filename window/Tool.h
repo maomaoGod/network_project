204c3e79bf51a191729c2509cca487113c4bc5e6
TySag <1394371281@qq.com>
2015-03-29 21:16:44 +0800

    Tools again
    the threads

--- window/Tool.h
+++ window/Tool.h
@@ -1,31 +1,59 @@
 //tools for other to use
 #include <cstring>
+#include <thread>
+#include <windows.h>
 #include "stdafx.h"
 #pragma once
-
+using namespace std;
 namespace Tools{
+	//overload : it don't need to use _T anymore
+    void AfxMessageBox(string Msg){
+		AfxMessageBox(Msg);
+	}
 	//Dllusr
 	class Dlluser{
 
 
 	public :
-		void Getpath(CString path){
+		void Getpath(string path){
 			this->path = path;
 		}
 
 	private:
-		CString path;
+		string path;
 	};
-
+	
 	//get html 
 	class Htmlget{
 	public:
-		void GetUrl(CString path){
+		void GetUrl(string path){
 			this->path = path;
 		}
-
+		string GetText(){
+			return this->document;
+		}
+		bool MainAsk(){
+			document = "";
+			//open a thread to Request
+			hThread = CreateThread(NULL, 0, this->Request, 0, 0, &thread_id);
+			if (hThread == NULL) {
+				AfxMessageBox(" 系统创建线程失败，没有获取信息\n ");
+				return false;//_T()
+			}
+			while (true){
+				if (document != "") break;
+			}
+			//destory
+			ExitThread(thread_id);
+			return true;
+		}
 	private:
-		CString path;
-
+		string path;
+		string document;
+		DWORD thread_id;
+		HANDLE hThread;
+		void Request(){
+			//set the document acconding to the path
+		}
 	};
 }
