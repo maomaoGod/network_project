a96029500e94722c032e5bbda0b2cab3826615ff
TySag <1394371281@qq.com>
2015-04-01 18:02:02 +0800

    NetTraffic
    TCP、UDP在该命名空间中实现

---
+++ window/ACMNet/ACMNet/NetTraffic.h
@@ -0,0 +1,17 @@
+#include "Tools.h"
+
+//there we will use the Socket class to transform the data
+using namespace Tools;
+
+namespace NetTraffic{
+	// the TCP link
+	class TCP{
+		Dlluser MySocket;//the socket we define from the dll
+
+	};
+	// the UDP link
+	class UDP{
+		Dlluser MySocket;//the socket we define from the dll
+
+	};
+}
