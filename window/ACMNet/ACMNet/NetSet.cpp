62657464351e9a5d79a17b528b288e0c959a0bf9
李贤亮 <834658870@qq.com>
2015-04-01 12:47:29 +0800

    新的框架程序
    新的框架程序
    NetDlg 主窗体
    NetSet 设置窗体

---
+++ window/ACMNet/ACMNet/NetSet.cpp
@@ -0,0 +1,31 @@
+// NetSet.cpp : 实现文件
+//
+#include "stdafx.h"
+#include "ACMNet.h"
+#include "NetSet.h"
+#include "afxdialogex.h"
+
+
+// NetSet 对话框
+
+IMPLEMENT_DYNAMIC(NetSet, CDialogEx)
+
+NetSet::NetSet(CWnd* pParent /*=NULL*/)
+	: CDialogEx(NetSet::IDD, pParent)
+{
+
+}
+
+NetSet::~NetSet()
+{
+}
+
+void NetSet::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(NetSet, CDialogEx)
+END_MESSAGE_MAP()
+
---
+++ window/ACMNet/ACMNet/NetSet.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "resource.h"
+
+// NetSet 对话框
+
+class NetSet : public CDialogEx
+{
+	DECLARE_DYNAMIC(NetSet)
+
+public:
+	NetSet(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~NetSet();
+
+// 对话框数据
+	enum { IDD = IDD_SET };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
