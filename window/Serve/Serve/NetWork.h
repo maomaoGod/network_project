1e4ebd6ad385fee3b8283c4906d9a4e548db82eb
SoryAsuka <liuyongbocap@163.com>
2015-05-05 17:10:48 +0800

    FTPV

--- window/Serve/Serve/NetWork.h
+++ window/Serve/Serve/NetWork.h
@@ -11,6 +11,7 @@
 *FILE TYPE + " " + FILE LEN + " " + FILE CONTENT
 */
 #pragma once
+#include<io.h>
 #include "map"
 #include "Tools.h"
 #include "MyServeSocket.h"
@@ -550,84 +551,27 @@ namespace NetWork{
 			return;
 		}
 		void CD(vector<string> data){
-			/*
-				int count = 0;
-				char **fileNameList = NULL;
-				struct dirent* ent = NULL;
-				DIR *pDir;
-				char dir[512];
-				struct stat statbuf;
+			struct _finddata_t files;
+			int File_Handle;
+			int i = 0;
+			string *temp;
+			data[1] = data[1] + "/*";
 
-				//打开目录
-				if ((pDir = opendir(path)) == NULL)
-				{
-				myLog("Cannot open directory:%s\n", path);
-				return NULL;
-				}
-				//读取目录
-				while ((ent = readdir(pDir)) != NULL)
-				{ //统计当前文件夹下有多少文件（不包括文件夹）
-				//得到读取文件的绝对路径名
-				snprintf(dir, 512, "%s/%s", path, ent->d_name);
-				//得到文件信息
-				lstat(dir, &statbuf);
-				//判断是目录还是文件
-				if (!S_ISDIR(statbuf.st_mode))
-				{
-				count++;
-				}
-				} //while
-				//关闭目录
-				closedir(pDir);
-				//  myLog("共%d个文件\n", count);
-
-				//开辟字符指针数组，用于下一步的开辟容纳文件名字符串的空间
-				if ((fileNameList = (char**)myMalloc(sizeof(char*)* count)) == NULL)
-				{
-				myLog("Malloc heap failed!\n");
-				return NULL;
-				}
-
-				//打开目录
-				if ((pDir = opendir(path)) == NULL)
-				{
-				myLog("Cannot open directory:%s\n", path);
-				return NULL;
-				}
-				//读取目录
-				int i;
-				for (i = 0; (ent = readdir(pDir)) != NULL && i < count;)
-				{
-				if (strlen(ent->d_name) <= 0)
-				{
-				continue;
-				}
-				//得到读取文件的绝对路径名
-				snprintf(dir, 512, "%s/%s", path, ent->d_name);
-				//得到文件信息
-				lstat(dir, &statbuf);
-				//判断是目录还是文件
-				if (!S_ISDIR(statbuf.st_mode))
-				{
-				if ((fileNameList[i] = (char*)myMalloc(strlen(ent->d_name) + 1))
-				== NULL)
-				{
-				myLog("Malloc heap failed!\n");
-				return NULL;
-				}
-				memset(fileNameList[i], 0, strlen(ent->d_name) + 1);
-				strcpy(fileNameList[i], ent->d_name);
-				myLog("第%d个文件:%s\n", i, ent->d_name);
-				i++;
-				}
-				} //for
-				//关闭目录
-				closedir(pDir);
-
-				*fileCount = count;
-				return fileNameList;
-				}
-				*/
+			File_Handle = _findfirst(data[1].c_str() , &files);
+			if (File_Handle == -1)
+			{
+				ErrorCode = OP_Fail;
+				return;
+			}
+			do
+			{
+				//printf("%s \n",files.name);
+				temp=new string(files.name);
+				RespondMsg = *temp + " ";
+			} while (0 == _findnext(File_Handle, &files));
+			_findclose(File_Handle);
+			//printf("Find %d files\n",i);
+			return;
 		}
 		void MKDIR(vector<string> data){
 			
