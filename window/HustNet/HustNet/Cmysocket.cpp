4a46981a97e91e4988cdaa440eed78347e3a7685
李贤亮 <834658870@qq.com>
2015-05-06 12:46:34 +0800

    忘记注释就推上去了,注释了未完成的部分
    忘记注释就推上去了

--- window/HustNet/HustNet/Cmysocket.cpp
+++ window/HustNet/HustNet/Cmysocket.cpp
@@ -5,7 +5,6 @@
 #include "Cmysocket.h"
 #include "HustNet.h"
 
-
 Cmysocket::Cmysocket()
 {
 }
@@ -14,51 +13,58 @@ Cmysocket::Cmysocket()
 Cmysocket::~Cmysocket()
 {
 }
-	struct sockstruct {
-		unsigned short  dstport;
-		unsigned short  bindport;
-		int    datalength;
-		char dstip[20];
-		char data[2048];
-	};
+
+/*
+struct sockstruct {
+unsigned short  dstport;
+unsigned short  bindport;
+int    datalength;
+char dstip[20];
+char data[2048];
+};
+
+*/
 
 int Send(const void* lpBuf, int nBufLen)
 {
-	sockstruct mysock;
+	 sockstruct mysock;
 	memcpy(mysock.data, lpBuf, nBufLen);
+	return nBufLen;
 }
 /** Udp发数据
 */
 int SendTo(const void* lpBuf, int nBufLen, UINT nHostPort, LPCTSTR lpszHostAddress)
 {
-	sockstruct mysock;
+	struct sockstruct mysock;
 	memcpy(mysock.data, lpBuf, nBufLen);
 	memcpy(mysock.dstip, lpszHostAddress, sizeof(lpszHostAddress));
 	mysock.dstport = nHostPort;
+	return nBufLen;
 }
 /** 接收数据
 */
 int    Receive(void* lpBuf, int nBufLen)
 {
-	sockstruct mysock;
-	memcpy(mysock.data, lpBuf, nBufLen);
+	struct sockstruct mysock;
+	 memcpy(mysock.data, lpBuf, nBufLen);
+	 return nBufLen;
 }
 
 /** 建立TCP连接
 */
 bool  Connect(LPCTSTR lpszHostAddress, UINT nHostPort)
 {
-
+	return true;
 }
 /** 监听连接请求
 */
 bool Listen()
 {
-
+	return true;
 }
 /** 创建socket
 */
 bool Create()
 {
-
+	return true;
 }
