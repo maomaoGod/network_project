e8d69f2a36dd6d3f85b9ef8081258769424e8d64
mathematicer <1724073126@qq.com>
2015-06-29 18:26:44 +0800

    Update My_linker.h

--- window/NetProtocol/NetProtocol/My_linker.h
+++ window/NetProtocol/NetProtocol/My_linker.h
@@ -19,6 +19,13 @@
 #define MAC_ADDR_SIZE 3
 typedef unsigned char Byte;
 
+/**
+*@class <CMlinker>
+*@brief 链路层数据段帧结构体
+*@author ACM2012
+*@note
+*链路层的发送帧数据部分结构，采用标准以太网帧结构
+*/
 struct Frame{
 	unsigned short MAC_des[MAC_ADDR_SIZE];           /**@brief MAC_dst MAC目标地址*/ 
 	unsigned short MAC_src[MAC_ADDR_SIZE];           /**@brief MAC_src MAC源地址*/ 
@@ -68,10 +75,10 @@ struct Frame{
 };
 
 /**
-*@brief 帧结构定义
+*@brief 广播帧帧结构定义
 *@author ACM2012
 *@note
-* 此处是帧结构的帧头的定义
+* 此处是广播帧结构的定义
 */
 struct Broadcast_frame
 {
@@ -83,7 +90,13 @@ struct Broadcast_frame
 	unsigned short CRC;
 };
 
-//帧头部结构体，共14字节
+/**
+*@class <CMlinker>
+*@brief 帧结构定义
+*@author ACM2012
+*@note
+*帧结构的定义，采用以太网标准帧结构
+*/
 struct EthernetHeader
 {
     u_char DestMAC[6];    //目的MAC地址 6字节
@@ -91,19 +104,32 @@ struct EthernetHeader
     u_short EthType;         //上一层协议类型，如0x0800代表上一层是IP协议，0x0806为arp  2字节
 };
 
-//28字节ARP帧结构
+/**
+*@class <CMlinker>
+*@brief ARP帧结构定义
+*@author ACM2012
+*@note
+*链路层ARP帧结构的定义，采用以太网标准ARP包结构
+*/
 struct Arpheader {
-	unsigned short HardwareType; //硬件类型
-	unsigned short ProtocolType; //协议类型
-	unsigned char HardwareAddLen; //硬件地址长度
-	unsigned char ProtocolAddLen; //协议地址长度
-	unsigned short OperationField; //操作字段
-	unsigned char SourceMacAdd[6]; //源mac地址
-	unsigned long SourceIpAdd; //源ip地址
-	unsigned char DestMacAdd[6]; //目的mac地址
-	unsigned long DestIpAdd; //目的ip地址
+	unsigned short HardwareType; /**@brief 硬件类型*/ 
+	unsigned short ProtocolType; /**@brief 协议类型*/
+	unsigned char HardwareAddLen; /**@brief 硬件地址长度*/
+	unsigned char ProtocolAddLen; /**@brief 协议地址长度*/
+	unsigned short OperationField; /**@brief 操作字段*/
+	unsigned char SourceMacAdd[6]; /**@brief 源mac地址*/
+	unsigned long SourceIpAdd; /**@brief 源ip地址*/
+	unsigned char DestMacAdd[6]; /**@brief 目的mac地址*/
+	unsigned long DestIpAdd; /**@brief 目的ip地址*/
 };
 
+/**
+*@class <CMlinker>
+*@brief 链路层类
+*@author ACM2012
+*@note
+*链路层类，其中包含链路层所涉及的各类函数以及各类变量
+*/
 class my_linker
 {
 private:
@@ -115,11 +141,11 @@ private:
 	};
 
 	static const int maxlength = 100000;
-	char **msg;						 //数据报
+	char **msg;						 
 	Data_Segment *buffer;
 	int bp;
 	int **data_pointer;
-	int *left;						 //每个数据报还剩多少帧
+	int *left;						 
 	void get_adapter();
 
 public:
@@ -176,23 +202,23 @@ public:
 		}
 		get_adapter();
 	}
-	char * combine(const u_char *);
-	int send_by_frame(IP_Msg *, pcap_t *, unsigned short, unsigned short);
+	char * combine(const u_char *);/**@brief 接收端通过接收到的帧结构组装网络层数据报的函数*/ 
+	int send_by_frame(IP_Msg *, pcap_t *, unsigned short, unsigned short);/**@brief 发送网络层传来的数据报的函数*/ 
 	int CSMA_CD_send(IP_Msg *, pcap_t *, unsigned short, unsigned short);
-	void GetSelfMac(char*, unsigned short *);
+	void GetSelfMac(char*, unsigned short *);/**@brief 获得自身MAC地址的函数*/ 
 	int ArpGetMacFromIp(pcap_t *adhandle, const char *ip_addr, unsigned char *ip_mac);
 
-	u_char* BuildArpPacket(unsigned short *, unsigned int, unsigned int);
+	u_char* BuildArpPacket(unsigned short *, unsigned int, unsigned int);/**@brief 构造ARP包的函数*/ 
 	bool check(const u_char *);
-	unsigned short crc16(unsigned char *, int);
-	unsigned short checkCrc16(unsigned char *, int);
+	unsigned short crc16(unsigned char *, int);/**@brief 计算CRC检验和的函数*/ 
+	unsigned short checkCrc16(unsigned char *, int);/**@brief 通过检验和检验接收的帧是否正确的函数*/ 
 	int pppEncode(unsigned char * buf, int len);/**@brief ppp数据帧编码函数*/ 
 	int pppDecode(unsigned char * buf, int len);/**@brief ppp数据帧解码函数*/ 
-	void send_broadcast(pcap_t  *adapterHandle, unsigned int src_IP, unsigned int dst_IP);
-	bool get_mac(pcap_t  *adapterHandle);
+	void send_broadcast(pcap_t  *adapterHandle, unsigned int src_IP, unsigned int dst_IP);/**@brief 发送广播帧*/ 
+	bool get_mac(pcap_t  *adapterHandle);/**@brief 获取MAC地址*/ 
 	bool transtable(unsigned int IP);
 	void packcap();
 	void Link2IP(WPARAM wparam);
 	static DWORD WINAPI NewPackThread(LPVOID lParam);
-	unsigned int getIP();
+	unsigned int getIP();/**@brief 获取IP地址*/ 
 };
