3549b7895dc9b6f7b258870e2aa16ee019732e04
Yan Wang <me@yanwang.me>
2015-06-29 22:43:02 +0800

    Update My_linker.h

--- window/NetProtocol/NetProtocol/My_linker.h
+++ window/NetProtocol/NetProtocol/My_linker.h
@@ -17,6 +17,16 @@
 #define table_size 10
 #define IP_SIZE sizeof(struct _iphdr)
 #define MAC_ADDR_SIZE 3
+
+
+#pragma pack(1)  //按一个字节内存对齐
+#define ETH_ARP         0x0806  //以太网帧类型表示后面数据的类型，对于ARP请求或应答来说，该字段的值为x0806
+#define ARP_HARDWARE    1  //硬件类型字段值为表示以太网地址
+#define ETH_IP          0x0800  //协议类型字段表示要映射的协议地址类型值为x0800表示IP地址
+#define ARP_REQUEST     1   //ARP请求
+#define ARP_REPLY       2      //ARP应答
+#define HOSTNUM         255   //主机数量
+
 typedef unsigned char Byte;
 
 /**
